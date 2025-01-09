85218ae99680d64db1eaad9771232d16a01c6ea5
Ella An <ella.an@mths.ca>
2025-01-09 11:19:51 -0500

    coded

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella and Maryam
+ * Created on: Jan 2025
+ * This program is a protype for our final project
 */
 
-basic.showString('Hello, World!')
+// variable
+let left = 0
+let right= 0
+
+// setup
+radio.setGroup(131)
+basic.showIcon(IconNames.Happy)
+
+// send
+input.onButtonPressed(Button.A, function () {
+    left = left + 1
+    radio.sendNumber(left)
+})
+
